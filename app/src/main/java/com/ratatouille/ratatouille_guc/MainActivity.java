30f49a33a58930827d06c8f333afbe622dafe7cd
Hady <hadyyasser23@gmai.com>
2017-11-29 14:54:13 -0500

    fix: welcome message crashes

--- app/src/main/java/com/ratatouille/ratatouille_guc/MainActivity.java
+++ app/src/main/java/com/ratatouille/ratatouille_guc/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ratatouille.ratatouille_guc;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,34 +11,46 @@ import com.google.gson.Gson;
 import com.stfalcon.chatkit.messages.MessageInput;
 import com.stfalcon.chatkit.messages.MessagesList;
 import com.stfalcon.chatkit.messages.MessagesListAdapter;
-
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Date;
+import java.util.Scanner;
+
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    MessagesList messagesList = null;
-    MessagesListAdapter<Message> adapter = null;
+    private MessagesList messagesList;
+    private static final String TAG = "HADY";
+    static MessagesListAdapter<Message> adapter = null;
     static String uuid = "";
     static Author author = null;
     static int msg_id = 0;
+    static Author me = new Author("1", "ratatouille", null);
 
     public static Response getReq(String urlString) throws IOException {
         URL url = new URL(urlString);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setConnectTimeout(7000);
+        conn.setRequestMethod("GET");
+
+//        Log.i(TAG, conn.getPermission().toString());
         conn.setRequestMethod("GET");
         BufferedReader is = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        Gson g = new Gson();
+
         String res = "";
         String cur = "";
         while((cur = is.readLine()) != null){
             res = res + cur;
         }
+        Gson g = new Gson();
+        System.out.println(res);
         return g.fromJson(res, Response.class);
     }
     public static void postReq(String message, String urlString) throws IOException {
@@ -56,12 +69,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            //your codes here
+
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MessageInput send = (MessageInput) findViewById(R.id.send);
-        messagesList = (MessagesList) findViewById(R.id.messagesList);
-
+        this.messagesList = (MessagesList) findViewById(R.id.messagesList);
+        MessageInput send = (MessageInput) findViewById(R.id.send);
         Response res = null;
 
         try {
@@ -72,8 +94,11 @@ public class MainActivity extends AppCompatActivity {
         uuid = res.uuid;
         author = new Author(uuid, "newUser", null);
 
-        adapter = new MessagesListAdapter<>(res.uuid, null);
-        messagesList.setAdapter(adapter);
+        adapter = new MessagesListAdapter<>(uuid, null);
+
+        this.messagesList.setAdapter(adapter);
+
+        adapter.addToStart(new Message((msg_id++)+"", me, res.message,new Date()), true);
 
         send.setInputListener(new MessageInput.InputListener() {
             @Override
